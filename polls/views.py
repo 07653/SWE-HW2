9489bbaa6f92efa2a51652233b6eeb4db19d2b2a
unknown <hl6129@nyu.com>
2025-03-07 15:09:24 -0500

    update

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from .models import Choice, Question
-from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
